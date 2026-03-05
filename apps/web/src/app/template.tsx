c9ec10235e644715e9862ddacff02343c9dd5b98
Ryan Waller <ryan@othermeans.us>
2026-03-05 14:15:38 -0500

    Remove page fade transition to eliminate load flashing

--- apps/web/src/app/template.tsx
+++ apps/web/src/app/template.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-
-let hasRenderedOnce = false;
-
 export default function Template({ children }: { children: React.ReactNode }) {
-  const shouldAnimate = hasRenderedOnce;
-
-  useEffect(() => {
-    hasRenderedOnce = true;
-  }, []);
-
-  return <div className={shouldAnimate ? "page-transition" : undefined}>{children}</div>;
+  return <>{children}</>;
 }
